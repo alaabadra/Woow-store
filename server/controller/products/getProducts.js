80c741f38395aaa1ddddc336be99c9624b02bbbd
Eng-Alaa <alaabadra4@gmail.com>
2019-12-30 18:27:42 -0500

    route for getproducts from result query db

--- server/controller/products/getProducts.js
+++ server/controller/products/getProducts.js
@@ -1,7 +1,22 @@
 
-module.exports=(req,res)=>{
-    res.status(200).send({
-        error:null,
-        data:'route getProduct success'
+const {getAllProducts} = require('../../database/queries/products/getProducts');
+
+module.exports =(req,res)=>{
+    getAllProducts()
+    .then(result=>{
+        console.log(6666,result.rows);
+        if(result.rowCount){
+            res.status(200).send({
+                error:null,
+                allPosts:result.rows
+            })
+        }else{
+            res.status(400).send(JSON.stringify({ msg: 'Internal ServerError' }));
+           
+        }
+        
     })
+    .catch(console.log)
+
+    
 }
