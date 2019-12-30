6e114c2cdb2491b9175b7334154a68068e90079a
Eng-Alaa <alaabadra4@gmail.com>
2019-12-30 18:28:14 -0500

    result query from db

---
+++ server/database/queries/products/getProducts.js
@@ -0,0 +1,9 @@
+const dbconnection = require('../../config/db_connection');
+
+const getAllProducts = () => {
+  const sql = {
+    text: 'SELECT * FROM products  '
+  };
+  return dbconnection.query(sql);
+};
+module.exports={getAllProducts}
