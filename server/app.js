f1b7fea625f648161226a3ba2c1dfacfbe9fee49
Eng-Alaa <alaabadra4@gmail.com>
2019-12-30 03:27:50 -0500

    edit in line router

--- server/app.js
+++ server/app.js
@@ -2,16 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const cookie = require('cookie-parser');
-
+const router = require('./routes');
 app.use(express.json());
 app.use(cookie());
-app.get('/api/v1', (req,res)=>{
-    res.status(200).send({
-        error:'null',
-        data:'route setup server success'
-    });
-    
-});
+app.use('/api/v1', router);
 
 module.exports = http;
 
