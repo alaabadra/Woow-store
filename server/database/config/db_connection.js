c43f8a69a15775d081eedfa1d88114408984f785
Eng-Alaa <alaabadra4@gmail.com>
2019-12-30 17:43:10 -0500

    create connecion database

---
+++ server/database/config/db_connection.js
@@ -0,0 +1,27 @@
+
+const {Pool} = require('pg');
+const {parse}=require('url');
+require('dotenv').config();
+let {DATABASE_URL}=process.env;
+const {
+MAX_DB_CONNECTION, NODE_ENV, hostname
+} = process.env;
+
+const params = parse(DATABASE_URL);
+console.log('ppp',params);
+const {
+hostname: host, port, pathname, auth
+} = params;
+const [user, password] = auth.split(':');
+const options = {
+host,
+port,
+user,
+password,
+database: pathname.split('/')[1],
+max: MAX_DB_CONNECTION || 2,
+ssl: hostname !== 'localhost'
+};
+console.log('ooo',options);
+const pool = new Pool(options);
+module.exports = pool;
