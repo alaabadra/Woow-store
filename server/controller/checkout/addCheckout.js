577d4256ac1ef8a3ee073b03edc64ae71ca4970c
Eng-Alaa <alaabadra4@gmail.com>
2019-12-30 03:19:50 -0500

    add routes for checkout

---
+++ server/controller/checkout/addCheckout.js
@@ -0,0 +1,7 @@
+
+module.exports=(req,res)=>{
+    res.status(200).send({
+        error:null,
+        data:'route addCheckout success'
+    })
+}
---
+++ server/controller/checkout/index.js
@@ -0,0 +1,7 @@
+const getCheckout = require('./getCheckout');
+const addCheckout = require('./addCheckout');
+
+module.exports = {
+    getCheckout,
+    addCheckout
+}
