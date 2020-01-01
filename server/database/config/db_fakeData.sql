45bd23ccb4b1aac6ef6ff44a0243f3249cc7ae0f
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 04:08:37 -0500

    upadate in file fake data sql

--- server/database/config/db_fakeData.sql
+++ server/database/config/db_fakeData.sql
@@ -1,35 +1,57 @@
 INSERT INTO
   customer (
-    id,
+    customer_id,
     firstname,
     lastname,
-    email
-
-      )
+    email,
+    password
+  )
 VALUES
   (
     1,
-    'alaa ',
+    'alaa',
     'badra',
-    'alaa@h.c'
-     
-  ),(
+    'alaabadra4@gmail.com',
+    '$2a$05$6Q1VE6DzhS8Z2pBr1VEPv.sM2dWPKacPv9GFC5M1.auGPlbab38aK'
+  ),
+  (
     2,
-    'dema ',
-    'badra',
-    'dema@h.c'
-     
+    'ala',
+    'tare',
+    'aa@hotmail.com',
+    '$2a$05$xuOTwN1iWrraW7Og7cVAWu00RUpEXg6AV8X3Mz8cIUoQcT952FJ7e'
+  ),
+  (
+    3,
+    'alaa',
+    'bad',
+    'dim@gmail.com',
+    '$2a$05$mjKkeAAMAaEsORbu.ZQKQurq4nr3pW3n524uwz9uQBrwLt3iGMAQG'
+  ),
+  (
+    4,
+    'ala',
+    'tare',
+    'bbb@hotmail.com',
+    '$2a$05$rZ6MiHZcvdMzVwTyJsyJf.o.CXVgP0xt72NO0JzPL3o4uYtSfgJ9C'
   );
 
 INSERT INTO
+  cart (cart_id, cart_name, customer_cart_id)
+VALUES
+  (1, 'cart fruit ', 1),
+  (2, 'cart veg', 2);
+
+INSERT INTO
   products (
-    id,
+    product_id,
     product_name,
     product_image,
     product_price,
     sizes,
-    customer_id
-      )
+    customer_id,
+    cart_product_id
+  )
 VALUES
   (
     1,
@@ -37,51 +59,32 @@ VALUES
     'garri.jpg',
     8.99,
     '50lbs',
+    1,
     1
-  ),(
+  ),
+  (
     2,
     'Brown Beans',
     'beans.jpeg',
     12.00,
     '2 lbs',
-    1
-  );
-
-  INSERT INTO
-  cart (
-    customer_id,
-    cart_name,
-    product_id
-
-      )
-VALUES
-  (
-    1,
-    'cart fruit ',
-     1
-  ),(
     2,
-    'cart veg',
     2
-  );
+  ),
+  (3, 'tomato', 'tomato.jpg', 8.99, '50lbs', 1, 1),
+  (4, 'tomato', 'tomato.jpg', 8.99, '50lbs', 1, 1),
+  (5, 'tomato', 'tomato.jpg', 8.99, '50lbs', 1, 1);
 
- INSERT INTO
+INSERT INTO
   transactions (
-    id,
+    id_p_transactions,
     price,
     status,
-    customer_id
-      )
+    customer_transactions_id,
+    num_card
+  )
 VALUES
-  (
-    1,
-    8.99,
-    'success',
-    2
-  ),(
-    2,
-    100,
-    'Failed',
-    2
-  );
+  (1, 8.99, 'success', 1, 213322),
+  (2, 100, 'Failed', 3, 656544);
 
+  
