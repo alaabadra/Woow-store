b8cf76ad261fc38c89c4dfca68e04442db5ed437
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 18:48:26 -0500

    add file login , check email and pass , put in data user in cookie

--- server/controller/authunticationLogin/1.authunticationLogin.js
+++ server/controller/authunticationLogin/1.authunticationLogin.js
@@ -4,23 +4,29 @@ const bcrypt = require('bcryptjs')
 module.exports = (req, res, next) => {
     const memberInfo = { ...req.body }
     if (memberInfo) {
+        console.log('memberinfo',memberInfo);
+        
         checkEmail(memberInfo.email)
             .then((result) => {
+                console.log('result',result.rows);
+                
                 if (result.rows[0]) {
                     bcrypt.compare(memberInfo.password, result.rows[0].password, (err, valid) => {
                         if (valid) {
-                            const { id, email } = { ...result.rows[0] }
-                            const userInfoEnc = { id, email };
-                           sign(userInfoEnc, process.env.SECRET,(err, result) => {
-                                if (err) return res.json({err});
-                               const ll = res.cookie('JWTcustomerId', result, {
+                            console.log('valid pass',valid);
+                            
+                            const { customer_id, email } = { ...result.rows[0] }
+                            const userInfoEnc = { customer_id, email };
+                            sign(userInfoEnc, process.env.SECRET, (err, result) => {
+                                if (err) return res.json({ err });
+                                res.cookie('JWTcustomerId', result, {
                                     maxAge: 60 * 60 * 24 * 30,
                                     httpOnly: true,
                                 });
-                                
+
                                 res.send({ error: null, data: result })
-                           } );
-                            
+                            });
+
                         }
                         else {
                             res.status(400).send(JSON.stringify({ msg: 'check your password' }))
