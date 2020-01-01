d1e33c84392d7854cc2acaa561da190619f0397f
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 18:49:37 -0500

    edit in index in ligin

--- server/controller/authunticationLogin/index.js
+++ server/controller/authunticationLogin/index.js
@@ -1,11 +1,9 @@
 const authunticationLogin = require('./1.authunticationLogin');
 const authunticatinVerify = require('./2.authunticatinVerify');
-const isAuthenticated = require('./3.isAuthenticated');
 const authunticationLogout = require('./authunticationLogout');
 
 module.exports={
     authunticationLogin,
     authunticatinVerify,
-    isAuthenticated,
     authunticationLogout
 }
