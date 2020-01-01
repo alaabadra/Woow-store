fe0a6449e4e9ed19ed4eedf97e9884ad0bb9d74b
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 06:39:25 -0500

    add route fot addUser

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const {getCart,addCart}=require('../controller/cart/index');
 const {getProducts,addProduct} = require('../controller/products/index');
 const {getNotifications,addNewNoification,patchNotifications} = require('../controller/notifications/index');
 const {getCheckout,addCheckout}=require('../controller/checkout/index');
-
+const {addUser}= require('../controller/signup/index')
 router.get('/get-cart',getCart);
 router.get('/add-cart',addCart);
 
@@ -16,5 +16,9 @@ router.get('/update-noti',patchNotifications);
 
 router.get('/get-checkout',getCheckout);
 router.get('/add-checkout',addCheckout);
+
+
+router.post('/add-user',addUser);
+
 
 module.exports=router;
