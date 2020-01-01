6f428aac69d820f2396674760a192ad758793ef6
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 15:49:53 -0500

    edit in calling this file

--- server/database/queries/products/getProductsCart.js
+++ server/database/queries/products/getProductsCart.js
@@ -1,12 +1,11 @@
 
 const dbconnection = require('../../config/db_connection');
 
-const getProductsCart = (cartProductId) => {
+module.exports= (cartProductId) => {
   const sql = {
     text: 'SELECT * FROM products  WHERE cart_product_id=$1',
     values:[cartProductId]
   };
   return dbconnection.query(sql);
 };
-module.exports={getProductsCart};
 
