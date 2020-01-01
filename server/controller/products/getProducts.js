c14b8a15bf37ff6bf93e219e235175b6c7865e09
Eng-Alaa <alaabadra4@gmail.com>
2020-01-01 18:53:43 -0500

    ad authuntication in all pages in website

--- server/controller/products/getProducts.js
+++ server/controller/products/getProducts.js
@@ -2,19 +2,27 @@
 const {getAllProducts} = require('../../database/queries/products/getProducts');
 
 module.exports =(req,res)=>{
-    getAllProducts()
-    .then(result=>{
-        if(result.rowCount){
-            res.status(200).send({
-                error:null,
-                AllProducts:result.rows
+    const { customer_id } = req.userInfoDec;
+        if (customer_id) {
+            res.send({ success: true, data: customer_id });
+            getAllProducts()
+            .then(result=>{
+                if(result.rowCount){
+                    res.status(200).send({
+                        error:null,
+                        AllProducts:result.rows
+                    })
+                }else{
+                    res.status(400).send(JSON.stringify({ msg: 'Internal ServerError' }));
+                   
+                }
+                
             })
-        }else{
-            res.status(400).send(JSON.stringify({ msg: 'Internal ServerError' }));
-           
+            .catch(() => next({ code: 500, msg: 'sorry , found Inernal server error' }));
+
+        } else {
+            res.status(401).send(JSON.stringify({ msg: 'you not authrized in this page' }))
         }
-        
-    })
-    .catch(() => next({ code: 500, msg: 'sorry , found Inernal server error' }));
+   
 }
 
--- server/controller/products/getProductsCart.js
+++ server/controller/products/getProductsCart.js
@@ -1,24 +1,32 @@
 
-const {getProductsCart} = require('../../database/queries/products/index');
+const { getProductsCart } = require('../../database/queries/products/index');
 
-module.exports =(req,res)=>{
-    const {cartProductId}=req.params;
-    getProductsCart(cartProductId)
-    .then(result=>{
-        if(result.rowCount){
-            res.status(200).send({
-                error:null,
-                allProductsThisCart:result.rows
+module.exports = (req, res) => {
+    const { cartProductId } = req.params;
+    const { customer_id } = req.userInfoDec;
+    if (customer_id) {
+        res.send({ success: true, data: customer_id });
+        getProductsCart(cartProductId)
+            .then(result => {
+                if (result.rowCount) {
+                    res.status(200).send({
+                        error: null,
+                        allProductsThisCart: result.rows
+                    })
+                } else {
+                    res.status(400).send(JSON.stringify({ msg: 'sorry , this cart id not exsit ' }));
+
+                }
+
             })
-        }else{
-            res.status(400).send(JSON.stringify({ msg: 'sorry , this cart id not exsit ' }));
-           
-        }
-        
-    })
-    .catch(() => {
-        res.status(500).send(JSON.stringify({ msg: 'sorry , Internal server error ' }));
+            .catch(() => {
+                res.status(500).send(JSON.stringify({ msg: 'sorry , Internal server error ' }));
 
-    });
+            });
+
+    } else {
+        res.status(401).send(JSON.stringify({ msg: 'you not authrized in this page' }))
+    }
+
 }
 
