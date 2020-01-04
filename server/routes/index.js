c7148b01f76d4e01486df975d6530eaf42a85603
Eng-Alaa <alaabadra4@gmail.com>
2020-01-03 22:42:35 -0500

    add routes for mongo and install monoogs

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,18 +4,31 @@ const {getProducts,getProductsCart} = require('../controller/products/index');
 const {getNotifications,addNewNoification,patchNotifications} = require('../controller/notifications/index');
 const {getCheckout,addCheckout}=require('../controller/checkout/index');
 const {generatePayment,getTransaction}=require('../controller/transaction/index');
+const {getProductsMongo} = require('../controller/products/MongoDb/getProducts');
+
 const {addUser}= require('../controller/signup/index');
 const { authunticationLogin, authunticatinVerify,authunticationLogout} = require('../controller/authunticationLogin/index')
+require('../database/MongoDb/config/db_connection');
+// require('../database/MongoDb/config/AllDataTransactions')();
+// require('../database/MongoDb/config/AllDataCart')();
+// require('../database/MongoDb/config/AllDataCusomer')();
+// require('../database/MongoDb/config/AllDataProduct')();
+
 
 router.post('/add-user',addUser);
 router.post('/login', authunticationLogin);
 router.use(authunticatinVerify)
 router.get('/logout',authunticationLogout)
+// postgress
 router.get('/get-products',getProducts);
 router.post('/add-to-cart',addAproductToCart);
 router.get('/get-all-products-from-cart/:cartProductId',getProductsCart);
 router.post('/generate-pay',generatePayment);
 router.get('/get-trans',getTransaction);
+
+
+// mongo
+router.get('/get-products-mongo',getProductsMongo);
 
 router.get('/get-noti',getNotifications);
 router.get('/add-noti',addNewNoification);
